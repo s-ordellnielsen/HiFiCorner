9272408fec78a282dc14a9b9a20549f56c2c772a
Mike <U10604@rts-365.dk>
2022-03-30 09:43:43 +0200

    JSON DATA  IN CATEGORIES NOW, make sure server run

--- src/scripts/api/showproducts.js
+++ src/scripts/api/showproducts.js
@@ -1,9 +1,8 @@
 const productsdiv = document.getElementsByClassName("products")[0]
 
-const pList = function (products) {
+function pList(products) {
     products.forEach(function (product) {
-        const output =
-            `<article>
+        const output = `<article>
         <h1>Name: ${product.name}</h1>
         <h2>id: ${product.id} </h2>
         <p>Brand: ${product.brand}</p>

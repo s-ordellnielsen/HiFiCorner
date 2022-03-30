f6bcc40ddf68f3037c1de7e5dbbcbe04e644c8b9
cookieman2002 <mcl2002@outlook.dk>
2022-03-30 10:19:03 +0200

    scripted beta

---
+++ src/scripts/modules/register.js
@@ -0,0 +1,42 @@
+var form = document.getElementById("register");
+var userName = document.getElementById("register_username");
+var password = document.getElementById("register_password");
+var secondPassword = document.getElementById("register_repeat");
+var email = document.getElementById("register_email");
+var checkbox = document.getElementById("register_checkbox");
+
+var validregex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+var errormessages = [];
+const errormessagesPlaceholder = document.getElementById("register_errormessage")
+
+form.addEventListener("submit", checker)
+
+
+function checker(event) {
+    event.preventDefault()
+    errormessages = []
+
+
+
+    if (!userName.value.lenght > 0) {
+        errormessages.push("Username too short")
+    }
+    if (!email.value.match(validregex)){
+        errormessages.push("Email is invalid")
+    }
+    if(password.value < 1){
+      errormessages.push("Password is too short")  
+    }
+    if(secondPassword.value < 1 || !secondPassword.value.match(password.value)){
+        errormessages.push("password is incorrect")
+    }
+
+    if(errormessages.length !== 0){
+        errormessagesPlaceholder.innerText = errormessages
+    }
+    else{
+        alert("du er registret")
+    }
+
+}

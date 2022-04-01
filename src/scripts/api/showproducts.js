8eaca1ae360a2f2937de104b2bb15042e87fe247
Mark Brodersen <U2405963@rts-365.dk>
2022-04-01 11:30:37 +0200

    Commit

--- src/scripts/api/showproducts.js
+++ src/scripts/api/showproducts.js
@@ -3,7 +3,7 @@ const productsdiv = document.getElementsByClassName("categories__list")[0]
 function pList(products) {
     products.forEach(function (product) {
         const output = `<article class="categories__listProduct">
-        <img src="${product.images[0]}"
+        <img src="http://localhost:3001/products/images${product.images[0]}">
         <h2>Name: ${product.name}</h2>
         <span>price: ${product.price}</span>
         </article>`

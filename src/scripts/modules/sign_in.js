c69089268e8565c2b2dc214ac50c918d89c14c89
cookieman2002 <mcl2002@outlook.dk>
2022-04-05 10:43:21 +0200

    update site

---
+++ src/scripts/modules/sign_in.js
@@ -0,0 +1,11 @@
+var loginForm = document.getElementById("login")
+var loginUsername = document.getElementById("login_username")
+var loginPassword = document.getElementById("login_password")
+
+
+loginForm.addEventListener("submit", login)
+
+function login(event){
+    event.preventDefault()
+    if(loginUsername.ariaValueMax.length !=  )
+}

720a33102d4f65f30b663d041c016f153c1bb5f8
cookieman2002 <mcl2002@outlook.dk>
2022-04-05 14:12:23 +0200

    Update fetch

--- src/scripts/modules/sign_in.js
+++ src/scripts/modules/sign_in.js
@@ -2,10 +2,17 @@ var loginForm = document.getElementById("login")
 var loginUsername = document.getElementById("login_username")
 var loginPassword = document.getElementById("login_password")
 
+function logindb(){
+    fetch("http://localhost:3001/customers", {
+        method: "GET",
+    })
+    .then((response) => console.log(response))
+}
+
 
 loginForm.addEventListener("submit", login)
 
 function login(event){
     event.preventDefault()
-    if(loginUsername.ariaValueMax.length !=  )
+    
 }

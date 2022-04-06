1b07f753004fb8ff6c31258bd24ac934147886c0
Cookieman2002 <mcl2002@outlook.dk>
2022-04-06 11:14:36 +0200

    Update fetch works

--- src/scripts/modules/sign_in.js
+++ src/scripts/modules/sign_in.js
@@ -3,11 +3,18 @@ var loginUsername = document.getElementById("login_username")
 var loginPassword = document.getElementById("login_password")
 
 function logindb(){
-    fetch("http://localhost:3001/customers", {
-        method: "GET",
-    })
-    .then((response) => console.log(response))
+fetch("http://localhost:3001/customers", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json; charset=UTF-8"
+    },
+    body: JSON.stringify()
+})
+.then((response) => response.json()).then(data => console.log(data))
+
 }
+logindb()
+
 
 
 loginForm.addEventListener("submit", login)

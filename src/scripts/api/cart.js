b05de39ca3a8594ed21e3328ce1fa6901374292e
Carl Abel <caweb.cph@gmail.com>
2022-04-20 10:46:41 +0200

    quick update

--- src/scripts/api/cart.js
+++ src/scripts/api/cart.js
@@ -1,88 +1,21 @@
-const cartDiv = document.getElementsByClassName("cartDiv")[0]
-const findListOfItems = document.querySelector(".list-of-items")
+let ls = localStorage.getItem('cartItem')
 
-fetch("http://localhost:3001/products")
-    .then(response => response.json())
-    .then(productsArray => renderAllProducts(productsArray))
-
-function renderAllProducts(productsArray) {
-    productsArray.forEach(product => renderOneProduct(product))
+if (ls) {
+    let cartItems = JSON.parse(ls)
+} else {
+    let cartItems = []
 }
 
-function renderOneProduct(products) {
-    const newElement = document.createElement("div")
-    newElement.className = "content"
-    newElement.innerHTML = `
-        <div class="cartDiv__product"
-            <div class="cartDiv_center"
-                <img src="http://localhost:3001${products.images[0]}"
-                <h2>${products.name}</h2>
-                <p> Price:  £${products.price}</p>
-                <button class="cartDiv__addButton">Add to cart</button>
-            </div>
-        </div>
-    `
-    cartDiv.appendChild(newElement)
+var amount = document.querySelector('').value
+var productId = document.querySelector('').value
+var color = document.querySelector('').value
+
+let item {
+    amount : amount,
+    productId : id,
+    color : color
 }
 
-fetch("http://localhost:3001/cartItems")
-    .then(response => response.json())
-    .then(cartItemsArray => {
-        cartArray = cartItemsArray;
-        renderAllCartItems(cartArray)
-    })
+cartItems.push(item)
 
-function renderAllCartItems(cartItemsArray) {
-    cartItemsArray.forEach(cartItem => renderCartItem(cartItem))
-}
-
-function renderCartItem(cartItem) {
-    const newLi = document.createElement("li")
-    newLi.innerHTML = `<p id="pTag"> ${cartItem.products.name}:  £${cartItem.products.price}
-    <button class="cardDiv__deleteButton"
-        <span>Remove</span>
-    </button>
-    </p>
-    `
-    findListOfItems.append(newLi)
-}
-
-//add Btn
-const addButton = newElement.querySelector(".cartDiv__addButton")
-addButton.addEventListener("click", event => {
-    findListOfItems.innerText = ""
-
-    fetch("http://localhost:3001/cartItems", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify({
-            cart_id: 1,
-            product_id: product.id
-        })
-    })
-        .then(response => response.json())
-        .then(newCartItem => {
-            cartArray.push(newCartItem);
-            renderAllCartItems(cartArray)
-        })
-})
-
-//remove Btn
-const removeButton = newLi.querySelector(".cardDiv__deleteButton")
-removeButton.addEventListener("click", event => {
-    newLi.remove()
-    fetch(`http://localhost:3001/cartItems/${cartItem.id}`, {
-        method: "DELETE"
-    })
-        .then(response => response.json())
-        .then(results => {
-            cartArray = results
-            findListOfItems.innerHTML = ""
-            renderAllCartItems(cartArray)
-        })
-})
-
-
+localStorage.setItem('cartItems')

0e53a925621a063894f5cd62324bc0f739827d93
Carl Abel <caweb.cph@gmail.com>
2022-04-20 12:03:12 +0200

    20 april

--- src/scripts/api/cart.js
+++ src/scripts/api/cart.js
@@ -1,19 +1,19 @@
 let ls = localStorage.getItem('cartItem')
-
+console.log("cartItem");
 if (ls) {
     let cartItems = JSON.parse(ls)
 } else {
     let cartItems = []
 }
 
-var amount = document.querySelector('.amount').value
-var productId = document.querySelector('.productId').value
-var color = document.querySelector('color').value
+var amount = document.querySelector('.productDecription__amount').value
+var productId = document.querySelector('.productDecription__id').value
+var color = document.querySelector('.productDecription__color').value
 
-let item {
-    amount : amount,
-    productId : id,
-    color : color
+let item = {
+    'amount' : amount,
+    'id' : productId,
+    'color' : color
 }
 
 cartItems.push(item)

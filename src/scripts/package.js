b9ced6b7e8b31a41c4c93586250bc5d5c79b378c
CarlAbel <caweb.cph@gmail.com>
2022-04-26 19:18:20 +0200

    package js update

--- src/scripts/package.js
+++ src/scripts/package.js
@@ -2,3 +2,12 @@ import packagePageBanner from './components/packageDOM.js';
 
 document.body.appendChild(())
 document.body.appendChild(packagePageBanner())
+function package.js"() {
+const element = document.createElement('')
+element.classList.add('wrapper')
+
+// Code here
+
+return element
+}
+document.body.appendChild(header())

ee670321b378f5f6e420589aa6521fbde32cbc3d
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-04-26 10:36:00 +0200

    add productDescription.js

---
+++ src/scripts/components/productDesc.js
@@ -0,0 +1,10 @@
+function productDesc(text) {
+    const element = document.createElement('p')
+    element.classList.add('productDescription__desc')
+    element.textContent = text
+    
+    // Code here
+    
+    return element
+}
+export default productDesc

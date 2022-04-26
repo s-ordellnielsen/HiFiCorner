be0553483288cce5165caf3dd03fc8dd16e3313c
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-04-26 13:06:47 +0200

    cartBreadcrump design

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,3 +1,16 @@
 import themeToggle from './components/themeToggle.js'
+import cartBreadcrump from './components/cartBreadcrump.js'
+import '../style/style.scss'
 
-document.body.appendChild(themeToggle())
+function cart() {
+	const element = document.createElement('div')
+	element.classList.add('wrapper')
+	
+	// Code here
+
+	element.appendChild(themeToggle())
+	element.appendChild(cartBreadcrump())
+	
+	return element
+}
+document.body.appendChild(cart())

139dd269c0a934ce709ada44d1b2b86c0e5cfe66
CarlAbel <caweb.cph@gmail.com>
2022-04-26 13:28:37 +0200

    packageDOM Update Scss

--- src/scripts/components/packageDOM.js
+++ src/scripts/components/packageDOM.js
@@ -8,7 +8,7 @@ function packagePage() {
     element.innerHTML = `
         <h1 class="packageSection__header">Package Deals</h1>
         <section class="packageSection">
-            <img src="${packageIcon}" alt="" />
+            <img class="packageSection__img" src="${packageIcon}" alt="" />
             <aside class="packageSection__aside">
                 <p class="packageSection__aside__boldTopText">Connected Speakers - Surround Sound</p>
                 <p class="packageSection__aside__middleText">Sound in every corner of your home</p>

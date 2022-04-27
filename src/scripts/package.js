afca7bb36586577f50bae79c05ef649512587a51
Carl Abel <caweb.cph@gmail.com>
2022-04-27 09:52:58 +0200

    update package.js

--- src/scripts/package.js
+++ src/scripts/package.js
@@ -1,4 +1,13 @@
 import packagePageBanner from './components/packageDOM.js';
 
-document.body.appendChild(())
+
+function packagePage() {
+    const element = document.createElement('')
+    element.classList.add('wrapper')
+    
+
+    element.appendChild(packagePageBanner())
+    
+    return element
+}
 document.body.appendChild(packagePageBanner())

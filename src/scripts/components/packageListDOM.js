a131b07b6ab153f414c1ad832a5dd53a3a860638
Carl Abel <caweb.cph@gmail.com>
2022-04-27 13:41:07 +0200

    setup for packageSectionList Style and fetch

--- src/scripts/components/packageListDOM.js
+++ src/scripts/components/packageListDOM.js
@@ -4,20 +4,20 @@ import '../../style/modules/_standardBtn.scss';
 function packagePageList() {
     const element = document.createElement('div')
     element.classList.add('wrapper')
-    
+
     element.innerHTML = `
-        <h1 class="packageSectionBanner__header">Package Deals</h1>
-        <section class="packageSectionBanner">
+        
+        <section class="packageSectionList">
             
-            <aside class="packageSectionBanner__aside">
-                <p class="packageSectionBanner__aside__boldTopText">Connected Speakers - Surround Sound</p>
-                <p class="packageSectionBanner__aside__boldMiddleText">Sound in every corner of your home</p>
-                <p class="packageSectionBanner__aside__boldBottomText">Surround Sound - Package Deals</p>
-            </aside>
+            <label class="packageSectionList__label">
+                <p class="packageSectionList__label__name">Connected Speakers - Surround Sound</p>
+                <p class="packageSectionList__label__boldMiddleText">Sound in every corner of your home</p>
+                <p class="packageSectionList__label__boldBottomText">Surround Sound - Package Deals</p>
+            </label>
         </section>
 
         `
-    
+
     return element
 }
 export default packagePageList

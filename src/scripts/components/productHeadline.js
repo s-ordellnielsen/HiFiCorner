6679dc4d6f93df5216af6fea6368e75f9bcad5a1
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-04-27 11:10:24 +0200

    add classname x-mobil to productHeadline.js

--- src/scripts/components/productHeadline.js
+++ src/scripts/components/productHeadline.js
@@ -2,6 +2,7 @@ function productHeadline(text) {
     const element = document.createElement('h1')
     element.classList.add('productDescription__name')
     element.classList.add('x-largeH1')
+    element.classList.add('x-mobil')
     
     element.innerHTML = text
     

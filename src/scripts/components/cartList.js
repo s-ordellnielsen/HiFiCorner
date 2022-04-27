3422e67bf0cf8f22da726229a3ea4bcf4f9a26d2
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-04-27 15:37:19 +0200

    initial cart list setup

--- src/scripts/components/cartList.js
+++ src/scripts/components/cartList.js
@@ -1,3 +1,6 @@
+import '../../style/modules/cartList.scss'
+import ghostImg from '../../images/cart-human.svg'
+
 export default function cartList() {
 	let element = document.createElement('section')
 	element.classList.add('cartList')
@@ -7,6 +10,8 @@ export default function cartList() {
 
 	if (cartItemsLs) {
 		cartItems = JSON.parse(cartItemsLs)
+
+		printCart(cartItems)
 	}
 	else {
 		let burnerItem = {
@@ -14,17 +19,54 @@ export default function cartList() {
 			id: 12345,
 			color: "Black"
 		}
+		let burnerItem2 = {
+			amount: 1,
+			id: 12344,
+			color: "Red"
+		}
 
 		cartItems.push(burnerItem)
+		cartItems.push(burnerItem2)
 
 		localStorage.setItem('cartItems', JSON.stringify(cartItems))
+
+		emptyCart()
 	}
 
 	console.log(cartItems);
 
-	element.innerHTML = `
+	function printCart(items) {
+		let totalPrice = 0
+		let itemCount = items.length
 
-	`
+		let cartItemsList = document.createElement('ul')
+		cartItemsList.classList.add('cartList__list')
+		items.forEach( function(elm) {
+			let item = document.createElement('li')
+			item.classList.add('cartList__listItem')
+
+			item.innerHTML = `
+				<img src="" alt="" />
+				<h3>${elm.id}</h3>
+			`
+
+			cartItemsList.appendChild(item)
+		})
+		element.appendChild(cartItemsList)
+	}
+
+	function emptyCart() {
+		element.classList.add('cartList--empty')
+		element.innerHTML = `
+			<h1 class="cartList__emptyHeader">Nothing to see here ... yet</h1>
+			<p class="cartList__emptyText">But take a look at our wide range of premium HiFi equipment</p>
+			<div class="cartList__emptyBtns">
+				<a href="../categories" class="cartList__emptyBtn">Browse Products</a>
+				<a href="#" class="cartList__emptyBtn cartList__emptyBtn--random">Find a random product</a>
+			</div>
+			<img src="${ghostImg}" alt="" class="cartList__emptyHero" />
+		`
+	}
 
 	return element
 }

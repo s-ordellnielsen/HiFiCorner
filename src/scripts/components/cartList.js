01e6483d4ddbdef35454e26f5512de6fbbe22a27
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-04-27 11:38:33 +0200

    created burner ls

--- src/scripts/components/cartList.js
+++ src/scripts/components/cartList.js
@@ -2,5 +2,29 @@ export default function cartList() {
 	let element = document.createElement('section')
 	element.classList.add('cartList')
 
+	let cartItemsLs = localStorage.getItem('cartItems')
+	let cartItems = []
+
+	if (cartItemsLs) {
+		cartItems = JSON.parse(cartItemsLs)
+	}
+	else {
+		let burnerItem = {
+			amount: 3,
+			id: 12345,
+			color: "Black"
+		}
+
+		cartItems.push(burnerItem)
+
+		localStorage.setItem('cartItems', JSON.stringify(cartItems))
+	}
+
+	console.log(cartItems);
+
+	element.innerHTML = `
+
+	`
+
 	return element
 }

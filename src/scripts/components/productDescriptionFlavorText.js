6b8e763269f6fe05dc918e69751568f3db7d2a4a
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-04-27 12:12:52 +0200

    add productDescriptionFlavorText.js

---
+++ src/scripts/components/productDescriptionFlavorText.js
@@ -0,0 +1,10 @@
+function productDescriptionFlavorText(text) {
+    const element = document.createElement('h3')
+    element.classList.add('productDescription__flavor_text')
+    
+    
+    element.innerHTML = text
+    
+    return element
+}
+export default productDescriptionFlavorText

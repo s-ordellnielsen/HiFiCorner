db9d8b67d2fc7a21343e06ed6ab6c0264e65dcd9
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-04-27 11:09:20 +0200

    add productDescriptionHeading.js

---
+++ src/scripts/components/productDescriptionRating.js
@@ -0,0 +1,59 @@
+function productDescriptionRating(product) {
+    const element = document.createElement('div')
+    element.classList.add('productDescription__rating')
+
+    let productDecriptionStarsAndRatingFigure = document.createElement("div")
+    productDecriptionStarsAndRatingFigure.classList.add("productDecription__starsAndRatingFigure")
+
+    let productDecriptionStars = document.createElement("div")
+    productDecriptionStars.classList.add("productDecription__stars")
+
+    //find antal reviews og regn gennemsnit og math round af
+
+
+
+    let productAvergageRating = ""
+
+    let productAmountOfReviews = ""
+
+    productDecriptionStarsAndRatingFigure.appendChild(productDecriptionStars)
+
+    let productDecriptionRatingFigure = document.createElement("p")
+    productDecriptionRatingFigure.classList.add("productDecription__ratingFigure")
+    productDecriptionRatingFigure.innerHTML = `
+    (
+        <span>${productAvergageRating}</span>
+    )
+    `
+
+    let productDecriptionReviewBasis = document.createElement("p")
+    productDecriptionReviewBasis.classList.add("productDecription__reviewBasis")
+    productDecriptionReviewBasis.innerHTML = `Based on <span
+    class="productDecription__totalReviews">${productAmountOfReviews}</span> reviews.
+    
+    `
+    productDecriptionStarsAndRatingFigure.appendChild(productDecriptionRatingFigure)
+    element.appendChild(productDecriptionStarsAndRatingFigure)
+    element.appendChild(productDecriptionReviewBasis)
+
+
+
+    
+    return element
+}
+export default productDescriptionRating
+
+/*
+<div class="productDescription__rating">
+            <div class="productDecription__stars">
+                <i data-feather="star"></i>
+                <i data-feather="star"></i>
+                <i data-feather="star"></i>
+                <i data-feather="star"></i>
+                <i data-feather="star"></i>
+            </div>
+            <p class="productDecription__ratingFigure"></p>
+            <p class="productDecription__reviewBasis">Based on <span
+                    class="productDecription__totalReviews">76</span> reviews.</p>
+        </div>
+        */

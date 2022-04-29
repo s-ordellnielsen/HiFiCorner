1dacb5a0dad5d1420088a5da17033d2a1d98ea11
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-04-29 09:20:34 +0200

    basic styling

--- src/scripts/components/fetchCartItem.js
+++ src/scripts/components/fetchCartItem.js
@@ -10,7 +10,6 @@ export default function fetchCartItem(id) {
 			}
 		})
 		.then(function (data) {
-			console.log(data)
 			return data
 		});
 }
--- src/scripts/components/printCart.js
+++ src/scripts/components/printCart.js
@@ -1,5 +1,6 @@
 import fetchCartItem from './fetchCartItem.js';
 import feather from 'feather-icons';
+import '../../style/modules/cartItem.scss';
 
 export default function printCart(items) {
 	let totalPrice = 0;
@@ -7,21 +8,30 @@ export default function printCart(items) {
 
 	let cartItemsList = document.createElement('ul');
 	cartItemsList.classList.add('cartList__list');
-	items.forEach(function (item) {
+	items.forEach(function (item, index) {
 		console.log(item.id);
 		let itemLi = document.createElement('li');
-		itemLi.classList.add('cartList__listItem');
+		itemLi.classList.add('listItem');
 		fetchCartItem(item.id).then((product) => {
 			itemLi.innerHTML = `
-			<h3>${product.name}</h3>
-			<p>€${product.price}</p>
-			<div className="amountBtns">
-				<button className="amountBtn amountBtn--minus">${feather.icons.minus.toSvg()}</button>
-				<input type="number" class="amount" id="amount" value="${item.amount}" />
-				<button>${feather.icons.plus.toSvg()}</button>
+			<h3 class="listItem__header">${product.name}</h3>
+			<p class="listItem__price">€${product.price}</p>
+			<div className="listItem__amountBtns">
+				<button class="listItem__amountBtn listItem__amountBtn--minus">${feather.icons.minus.toSvg()}</button>
+				<input type="number" class="listItem__amount" id="amount" value="${
+					item.amount
+				}" />
+				<button class="listItem__amountBtn listItem__amountBtn--plus">${feather.icons.plus.toSvg()}</button>
 			</div>
-			<p>€${product.price * item.amount}</p>
+			<p class="listItem__total">€${product.price * item.amount}</p>
 		`;
+			itemLi
+				.querySelector('.listItem__amount')
+				.addEventListener('change', function () {
+					item.amount = parseInt(this.value)
+					items[index] = item
+					console.log(item)
+				});
 		});
 
 		cartItemsList.appendChild(itemLi);

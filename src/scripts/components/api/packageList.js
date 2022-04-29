7407ba56719bfd3ac3b4a399bccdb108170d53d6
CarlAbel <caweb.cph@gmail.com>
2022-04-29 13:25:33 +0200

    ratings and package update

--- src/scripts/components/api/packageList.js
+++ src/scripts/components/api/packageList.js
@@ -13,11 +13,12 @@ function packageList() {
                 article.innerHTML = `
         <div class="packageSectionList__container">
         <a class="packageSectionList__aTag" href="cart.html">
-            <img class="landing__heroProductImage" src="http://localhost:3001${itemPackage.images[0]}">
+            <img class="packageSectionList__img" src="http://localhost:3001${itemPackage.images[0]}">
             <div>
                 <h2 class="landing__heroProductBody">${itemPackage.name}</h2>
                 <p>${itemPackage.flavor_text}</p>
-                <span>${itemPackage.price}</span>
+                <span>${itemPackage.price}£</span>
+                <a class="packageSectionList__seeMoreBtn standardBtn btn"href="./packages?=id${element.id}">See more</a>
             </div>
         </a>
         </div>

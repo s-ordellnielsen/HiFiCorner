f29af4bfa972056631ed3abd2bd4d8ffba6243e4
Mark Brodersen <U2405963@rts-365.dk>
2022-04-29 09:53:23 +0200

    Last change

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -1,13 +1,11 @@
 import "../../style/style.scss"
 import '../../style/modules/_themeToggle.scss'
-import burgerToggle from "../modules/burgerToggle.js"
+import burgerToggle from "./burgerToggle.js"
 
-function header() {
+export default function header() {
     const element = document.createElement('div')
     element.classList.add('wrapper')
     element.appendChild(burgerToggle())
 
     return element;
 }
-
-document.body.append(header());

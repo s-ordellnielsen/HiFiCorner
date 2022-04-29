3aaea1ff99ddf6047c1be5a2b3cec2a53d3b2515
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-04-29 12:53:58 +0200

    add productSpecsReviewArticle.js

---
+++ src/scripts/components/productSpecsReviewArticle.js
@@ -0,0 +1,9 @@
+function productSpecsReviewArticle(product) {
+    const element = document.createElement('article')
+    element.classList.add('productSpecs__reviewsArticle')
+    
+    // Code here
+    
+    return element
+}
+export default productSpecsReviewArticle

dd1da92c740b1016d4810f6ae0fe8d0fa1a75230
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-05-02 13:40:34 +0200

    add inputID and productID

--- src/scripts/components/productMain.js
+++ src/scripts/components/productMain.js
@@ -36,6 +36,15 @@ function productMain(id) {
     .then(function (product) {
 
         document.title += "HiFi Corner - " + product.name
+
+        //input:hidden med id som value
+        let inputID = document.createElement("input")
+        inputID.type = "hidden"
+        inputID.value = product.id
+        console.log("inputID", inputID)
+        let productID = inputID.value
+
+        console.log("productID", productID)
         
         sectionProductDescription.appendChild(productHeadline(product.name))
         sectionProductDescription.appendChild(productGallery(product))

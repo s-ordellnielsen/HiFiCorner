a8e978113b38eea316e5b890e03a2beb1b5e9b18
Mark Brodersen <U2405963@rts-365.dk>
2022-05-02 08:32:58 +0200

    Quick commit for coop grunde

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import footerElement from "./components/footerElement.js"
 import brandSection from "./components/modules/brandSection.js"
 import header from "./components/header.js"
 import newsLetter from "./components/newsLetter.js"
+import packageSection from "./components/modules/parkageSection.js"
 
 import "../style/style.scss"
 import "../style/modules/pageFooter.scss"
@@ -14,6 +15,7 @@ function landing() {
     // Code here
     element.appendChild(header())
     element.appendChild(heroDom())
+    element.appendChild(packageSection())
     element.appendChild(brandSection())
     element.appendChild(newsLetter())
     element.appendChild(footerElement())

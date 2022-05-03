8e17b77e9041ac2bf3b32bc8f01a1fce5be28210
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-05-03 14:07:51 +0200

    switch to radio buttons

--- src/scripts/components/deliveryForm.js
+++ src/scripts/components/deliveryForm.js
@@ -3,7 +3,9 @@ import upsLogo from '../../images/deliveryOptions/UPS-logo.png'
 import fedExLogo from '../../images/deliveryOptions/FedEx-logo.png'
 import royalMailLogo from '../../images/deliveryOptions/Royal-Mail-Logo.png'
 import woltLogo from '../../images/deliveryOptions/wolt-logo.png'
+import deliveryFormvalidator from './deliveryFormvalidator.js'
 import feather from 'feather-icons'
+
 import '../../style/modules/deliveryOption.scss'
 
 export default function deliveryForm() {
@@ -33,7 +35,7 @@ export default function deliveryForm() {
 		</div>
 
 		<section class="deliveryOptions">
-			<article class="deliveryOption">
+			<label class="deliveryOption">
 				<div class="deliveryOption__logo">
 					<img src="${upsLogo}" alt="" class="deliveryOption__logoBlur"/>
 					<img src="${upsLogo}" alt="" class="deliveryOption__logoImage"/>
@@ -41,9 +43,10 @@ export default function deliveryForm() {
 				<h4 class="deliveryOption__name">UPS Express</h4>
 				<p class="deliveryOption__duration">1-day delivery</p>
 				<p class="deliveryOption__price">€14.99</p>
-				<button class="deliveryOption__submit">Use UPS ${feather.icons['arrow-right'].toSvg()}</button>
-			</article>
-			<article class="deliveryOption">
+				<!-- <button class="deliveryOption__submit">Use UPS ${feather.icons['arrow-right'].toSvg()}</button> -->
+				<input type="radio" name="dOptions" class="deliveryOption__radio"/>
+			</label>
+			<label class="deliveryOption">
 				<div class="deliveryOption__logo">
 					<img src="${fedExLogo}" alt="" class="deliveryOption__logoBlur"/>
 					<img src="${fedExLogo}" alt="" class="deliveryOption__logoImage" />
@@ -51,9 +54,10 @@ export default function deliveryForm() {
 				<h4 class="deliveryOption__name">FedEx</h4>
 				<p class="deliveryOption__duration">1-2 days</p>
 				<p class="deliveryOption__price">€3.99</p>
-				<button class="deliveryOption__submit">Use FedEx ${feather.icons['arrow-right'].toSvg()}</button>
-			</article>
-			<article class="deliveryOption">
+				<!-- <button class="deliveryOption__submit">Use FedEx ${feather.icons['arrow-right'].toSvg()}</button> -->
+				<input type="radio" name="dOptions" class="deliveryOption__radio"/>
+			</label>
+			<label class="deliveryOption">
 				<div class="deliveryOption__logo">
 					<img src="${royalMailLogo}" alt="" class="deliveryOption__logoBlur" />
 					<img src="${royalMailLogo}" alt="" class="deliveryOption__logoImage" />
@@ -61,9 +65,10 @@ export default function deliveryForm() {
 				<h4 class="deliveryOption__name">Royal Mail</h4>
 				<p class="deliveryOption__duration">1-5 days</p>
 				<p class="deliveryOption__price">Free</p>
-				<button class="deliveryOption__submit">Use Royal Mail ${feather.icons['arrow-right'].toSvg()}</button>
-			</article>
-			<article class="deliveryOption">
+				<!-- <button class="deliveryOption__submit">Use Royal Mail ${feather.icons['arrow-right'].toSvg()}</button> -->
+				<input type="radio" name="dOptions" class="deliveryOption__radio"/>
+			</label>
+			<label class="deliveryOption">
 				<div class="deliveryOption__logo">
 					<span>${feather.icons.heart.toSvg()} Green Delivery!</span>
 					<img src="${woltLogo}" alt="" class="deliveryOption__logoBlur deliveryOption__logoBlur--wolt"/>
@@ -72,10 +77,27 @@ export default function deliveryForm() {
 				<h4 class="deliveryOption__name">Wolt Bike</h4>
 				<p class="deliveryOption__duration">10-15 days</p>
 				<p class="deliveryOption__price">€1.99</p>
-				<button class="deliveryOption__submit">Use Wolt Bike ${feather.icons['arrow-right'].toSvg()}</button>
-			</article>
+				<!-- <button class="deliveryOption__submit">Use Wolt Bike ${feather.icons['arrow-right'].toSvg()}</button> -->
+				<input type="radio" name="dOptions" class="deliveryOption__radio"/>
+				<div class="deliveryOption__button">
+					Use Wolt Bike
+					<div>${feather.icons.check.toSvg()}</div>
+				</div>
+			</label>
 		</section>
 	`
+
+	element.querySelector('.deliveryOptions').addEventListener('click', function(e) {
+		element.querySelectorAll('.deliveryOption').forEach(function(elm) {
+			elm.classList.remove('deliveryOption--checked')
+		})
+
+		console.log(e.target)
+
+		e.target.closest('label').classList.add('deliveryOption--checked')
+	})
+
+	element.addEventListener('submit', deliveryFormvalidator)
 
 	return element
 }

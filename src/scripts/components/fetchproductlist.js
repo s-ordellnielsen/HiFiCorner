f02517a55ded9c721fdb61a791863ae693979687
cookieman2002 <mcl2002@outlook.dk>
2022-05-03 14:06:11 +0200

    html in js

--- src/scripts/components/fetchproductlist.js
+++ src/scripts/components/fetchproductlist.js
@@ -4,26 +4,35 @@ function fetchproducts() {
     element.classList.add('productsectionlist')
 
     fetchproductList()
-    .then(products => {
-        products.forEach(products => {
-            const output = document.createElement("article")
-            output.classList.add("productsectionlist__article")
-            output.innerHTML = `<div class="productsectionlist__container">
-                <img src="http://localhost3001${}" alt="" />
-            </div>`
+        .then(products => {
+            products.forEach(products => {
+                const output = document.createElement("article")
+                output.classList.add("productsectionlist__article")
+                output.innerHTML = `<div class="productsectionlist__container">
+                <img src="http://localhost3001${products.images[0]}" alt="" />
+            </div>
+            <div>
+                <h2>${products.name}</h2>
+                <p>${products.brand}</p>
+                <span>${products.price}£</span>
+                <a href="">${products.id}</a>
+                </div>
+                
+            `
+                element.appendChild(output)
+            })
         })
-    })
     // html stopper her
 
 
     var productlink = element.querySelector("productsectionlist__container")
 
-    productlink.addEventListener("click", function(){
+    productlink.addEventListener("click", function () {
         console.log("hej")
     })
 
     // Code here
-    
+
     return element
 }
 export default fetchproducts
--- src/scripts/productlist.js
+++ src/scripts/productlist.js
@@ -4,11 +4,11 @@ function productlist() {
     const element = document.createElement('div')
     element.classList.add('wrapper')
     
-    element.appendChild(fetchproductList())
-
-
+    
+    
     // Code here
     
     return element
 }
+document.body.appendChild(fetchproductList())
 document.body.appendChild(productlist())

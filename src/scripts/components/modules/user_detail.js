41a43053f68db55c9a0f4e67ef413f9f340f8ed9
cookieman2002 <mcl2002@outlook.dk>
2022-05-03 13:42:23 +0200

    Fix Fetch to update info

--- src/scripts/components/modules/user_detail.js
+++ src/scripts/components/modules/user_detail.js
@@ -7,7 +7,6 @@ function profileDetail() {
     const element = document.createElement('div')
     element.classList.add('profile_details')
     var data = JSON.parse(window.sessionStorage.getItem('user'))
-    console.log(data)
     
     element.innerHTML = `
     <button class="change">${feather.icons["edit-3"].toSvg()}</button>
@@ -30,14 +29,14 @@ function profileDetail() {
     
 
         <form class="profile__information">
-    <input type="text" value="${data.first_name}"  placeholder="Name" />
-    <input type="text" value="${data.last_name}"  placeholder="last name"/>
-    <input type="text" value="${data.email}"  placeholder="email"/>
-    <input type="text" value="${data.phone}"  placeholder="Phone number"/>
-    <input type="text" value="${data.address.country}"  placeholder="country"/>
-    <input type="text" value="${data.address.zip_code}"  placeholder="Zip-Code"/>
-    <input type="text" value="${data.address.city}"  placeholder="city"/>
-    <input type="text" value="${data.address.number}"  placeholder="Adress number"/>
+    <input type="text" name="firstname" value="${data.first_name}"  placeholder="Name" />
+    <input type="text" name="lastname" value="${data.last_name}"  placeholder="last name"/>
+    <input type="text" name="email" value="${data.email}"  placeholder="email"/>
+    <input type="text" name="phone" value="${data.phone}"  placeholder="Phone number"/>
+    <input type="text" name="country" value="${data.address.country}"  placeholder="country"/>
+    <input type="text" name="zipcode" value="${data.address.zip_code}"  placeholder="Zip-Code"/>
+    <input type="text" name="city" value="${data.address.city}"  placeholder="city"/>
+    <input type="text" name="addressnumber" value="${data.address.number}"  placeholder="Adress number"/>
 </form>  
 
     `
@@ -45,6 +44,9 @@ function profileDetail() {
     
     var test = element.querySelector(".change")
     var edit = element.querySelector(".profile__information")
+    var changer = true
+    console.log(data.id)
+    
 
     var favoritesElement = element.querySelector(".profile_favorites")
     data.favorites.forEach(function(favorite){
@@ -56,9 +58,37 @@ function profileDetail() {
     })
 
     test.addEventListener("click", function(){
+        changer = !changer
         edit.classList.toggle("profile__information--edit")
         test.classList.toggle("change--active")
+        
+        if(changer){
+            var updateddata = {
+                first_name: edit.firstname.value,
+                last_name: edit.lastname.value,
+                email: edit.email.value,
+                phone: edit.phone.value,
+                address: {
+                    city: edit.city.value,
+                    country: edit.country.value,
+                    zip_code: edit.zipcode.value,
+                    number: edit.addressnumber.value
+                }
+                
+            }
+            console.log(`http://localhost:3001/customers/${data.id}`)
+            console.log(updateddata)
+            fetch(`http://localhost:3001/customers/${data.id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type":"application/json; charset=UTF-8"
+                },
+                body: JSON.stringify(updateddata)
 
+            }).then(response => response.json())
+            .then(data => console.log(data))
+
+        }
         
     })
 

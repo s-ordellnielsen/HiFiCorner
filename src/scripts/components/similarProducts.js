3163e36f7f8b7ee335dda59d07187fb78c18aa23
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-05-04 12:40:40 +0200

    Add attemt at fetching similarProducts

--- src/scripts/components/similarProducts.js
+++ src/scripts/components/similarProducts.js
@@ -1,9 +1,78 @@
 import "../../style/base/_variables.scss";
 
-function similarProducts() {
+function similarProducts(product) {
     const element = document.createElement('section')
     element.classList.add('similarProducts')
-    element.innerHTML = `
+
+    console.log("type", product.type)
+
+    let productType = product.type
+
+    let simProdH2 = document.createElement("h2")
+    simProdH2.classList.add("similarProducts__header")
+    
+
+    fetch("http://localhost:3001/products?type="+productType+"&_limit=3", {
+        mode: 'cors',
+        headers: {
+            'Access-Control-Allow-Origin': ' * '
+        }
+    })
+    .then(function (response) {
+        if (response.status !== 200) {
+            console.log('fejl')
+            return []
+        }
+        return response.json()
+    })
+    .then(function (similarProducts) {
+
+        console.log("simProd", similarProducts)
+
+        similarProducts.forEach((element) => {
+            document.createElement()
+        })
+
+        
+
+        /*let types = []
+
+        db.forEach((product) => {
+            
+            types = types + product.type + ", "
+
+            types.forEach((typeName, similarTypes) => {
+        
+                if (typeName == productType){
+                    let simdesc = product.desc
+                    console.log("simdesc", simdesc)
+                }
+            })
+
+        })
+        console.log("allTypes", types);
+        
+       
+
+    
+
+        //console.log("slice", types.slice(",")); 
+
+        /*
+        if (types == productType){
+
+        }
+        */
+
+
+    })
+
+
+
+
+
+
+    /*element.innerHTML = `
     <h2 class="similarProducts__header">Products like this</h2>
             <div class="similarProducts__images">
                 <div class="similarProducts__currentImageDiv">
@@ -31,7 +100,7 @@ function similarProducts() {
             </div>
     
     `
-    
+    */
     // Code here
     
     return element

5fa28e8f846ee454b88ab1c0a8735ffca25f0d42
Mark Brodersen <markbrodersen49@gmail.com>
2022-05-04 10:09:51 +0200

    commit

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -4,9 +4,17 @@ function checkout() {
     const element = document.createElement('form')
     element.classList.add('checkout__form')
     element.appendChild(header())
-    element.appendChild(cartBreadcrump()[""])
+    element.appendChild(cartBreadcrump()["success", "success", "active"])
 
     element.innerHTML = `
+    <input type="text" name="" id="" placeholder="Full Name">
+    <input type="text" name="" id="" placeholder="Card Number">
+    <div>
+        <input type="text" name="" id="" placeholder="Expire Month">
+        <input type="text" name="" id="" placeholder="Expire Year">
+        <input type="text" name="" id="" placeholder="CVV">
+    </div>
+
     
     `
     // Code here

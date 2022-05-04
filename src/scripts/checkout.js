c2e7d49ecc792f0fc227c18a88e80fd02568e18a
Mark Brodersen <markbrodersen49@gmail.com>
2022-05-04 09:26:38 +0200

    cartBreedcrump half setup

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -1,10 +1,11 @@
 import header from "./components/header.js"
-
+import cartBreadcrump from "./components/cartBreadcrump.js"
 function checkout() {
     const element = document.createElement('form')
     element.classList.add('checkout__form')
     element.appendChild(header())
-    
+    element.appendChild(cartBreadcrump()[""])
+
     element.innerHTML = `
     
     `

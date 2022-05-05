87d72e02e5101cd0322099f3c0d0dcc018e8b29f
Mark Brodersen <U2405963@rts-365.dk>
2022-05-05 09:08:13 +0200

    Check

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -1,13 +1,11 @@
 import header from "./components/header.js"
 import cartBreadcrump from "./components/cartBreadcrump.js"
 function checkout() {
-    const element = document.createElement('Main')
-    element.classList.add('checkout__container')
-    element.appendChild(header())
-    element.appendChild(cartBreadcrump()["success", "success", "active"])
+    const element = document.createElement('form')
+    element.classList.add('checkout__form')
+    element.appendChild(cartBreadcrump(['success', 'success', 'active']))
 
     element.innerHTML = `
-    <form action="" class="checkout__form">
         <input type="text" name="" class="checkout__formNameInput" id="checkout__formNameInput" placeholder="Full Name">
         <input type="text" name="" class="checkout__formCardInput" id="checkout__formCardInput" placeholder="Card Number">
         <div>
@@ -16,13 +14,12 @@ function checkout() {
             <input type="text" name="" class="checkout__formCVVInput" id="checkout__formCVVInput" placeholder="CVV">
         </div>
         <input type="submit" class="checkout__formSubmit" id="checkout__formSubmit" value="Confirm Purchase">
-    </form>
-
-    
     `
     // Code here
 
 
     return element
 }
+document.body.append(header())
+document.body.append(cartBreadcrump(["success", "success", "active"]))
 document.body.append(checkout())

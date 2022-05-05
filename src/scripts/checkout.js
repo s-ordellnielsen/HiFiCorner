8a35a6d1e16b47578d9ef95cba91a19e7314e02f
Mark Brodersen <U2405963@rts-365.dk>
2022-05-05 08:38:16 +0200

    Changed class and id names

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -1,24 +1,27 @@
 import header from "./components/header.js"
 import cartBreadcrump from "./components/cartBreadcrump.js"
 function checkout() {
-    const element = document.createElement('form')
-    element.classList.add('checkout__form')
+    const element = document.createElement('Main')
+    element.classList.add('checkout__container')
     element.appendChild(header())
     element.appendChild(cartBreadcrump()["success", "success", "active"])
 
     element.innerHTML = `
-    <input type="text" name="" id="" placeholder="Full Name">
-    <input type="text" name="" id="" placeholder="Card Number">
-    <div>
-        <input type="text" name="" id="" placeholder="Expire Month">
-        <input type="text" name="" id="" placeholder="Expire Year">
-        <input type="text" name="" id="" placeholder="CVV">
-    </div>
+    <form action="" class="checkout__form">
+        <input type="text" name="" class="checkout__formNameInput" id="checkout__formNameInput" placeholder="Full Name">
+        <input type="text" name="" class="checkout__formCardInput" id="checkout__formCardInput" placeholder="Card Number">
+        <div>
+            <input type="text" name="" class="checkout__formMonthInput" id="checkout__formMonthInput" placeholder="Expire Month">
+            <input type="text" name="" class="checkout__formYearInput" id="checkout__formYearInput" placeholder="Expire Year">
+            <input type="text" name="" class="checkout__formCVVInput" id="checkout__formCVVInput" placeholder="CVV">
+        </div>
+        <input type="submit" class="checkout__formSubmit" id="checkout__formSubmit" value="Confirm Purchase">
+    </form>
 
     
     `
     // Code here
-    
+
 
     return element
 }
